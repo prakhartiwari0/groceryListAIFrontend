5826a06d81b0324a1991296cd80a7f2907b6178c
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-09-18 22:49:02 +0530

    Init

---
+++ src/views/MainView.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="main-container">
+
+
+    <section class="itemsListContainer">
+
+
+
+    </section>
+
+    <!-- Background Image -->
+    <img src="../assets/images/brand/foodImage-transparent.png"
+         alt="Food Image"
+         class="foodImage">
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.main-container {
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.itemsListContainer {
+  position: relative;
+  z-index: 1;
+}
+
+.foodImage {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-width: 25rem;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+  z-index: -1;
+  opacity: 0.7;
+  pointer-events: none;
+}
+</style>
